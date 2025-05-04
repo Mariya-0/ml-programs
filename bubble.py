66a4097735fe7a7578a1a0e46db24c2eb142dcf6
Mariya-0 <mariabashir7.mb@gmail.com>
2025-05-04 16:21:44 +0530

    try commit

--- bubble.py
+++ bubble.py
@@ -16,3 +16,21 @@ bubblesort(arr, size)
 
 print('The array after sorting (bubble sort) in Ascending order is:')
 print(arr)
+print("bubble sort")
+
+def bubblesort(array, size):
+    for i in range(size):
+        swapped = False
+        for j in range(size - i - 1):
+            if array[j] < array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swapped = True
+        if not swapped:
+            break  # If no elements were swapped, the array is already sorted
+
+arr = [-2, 45, 0, 11, -9, 88, -97, -202, 747]
+size = len(arr)
+bubblesort(arr, size)
+
+print('The array after sorting (bubble sort) in Descending order is:')
+print(arr)
